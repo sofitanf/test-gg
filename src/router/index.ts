5e12edfd7a27a6f0e777bff4500c4f4bc870cbb9
Sofita Nur Fitriana <fitahas@gmail.com>
2024-09-01 04:44:23 +0700

    add: scroll to top

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,9 @@ const router = createRouter({
       component: () => import('../views/Cart.vue'),
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
 });
 
 export default router;
